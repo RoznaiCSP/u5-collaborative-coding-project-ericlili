2d7b893feb8cf3f377494ef39daeb80770cc6094
LiliRuiz200 <120490999+LiliRuiz200@users.noreply.github.com>
2023-01-24 09:32:11 -0600

    add sprite

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
 // Don't forget to comment your code as you work!
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 2 2 2 2 . . . . . . . 
+    . . . 2 2 2 2 2 2 2 2 2 . . . . 
+    . . . e e e d d d f d . . . . . 
+    . . e d e d d d d f d d d . . . 
+    . . e d e e d d d d f d d d . . 
+    . . e e d d d d d f f f f . . . 
+    . . . . d d d d d d d d . . . . 
+    . . . 2 2 8 2 2 2 2 . . . . . . 
+    . . 2 2 2 8 2 2 8 2 2 2 . . . . 
+    . 2 2 2 2 8 2 2 8 2 2 2 2 . . . 
+    . 1 1 2 8 5 8 8 5 8 2 1 1 . . . 
+    . 1 1 1 8 8 8 8 8 8 1 1 1 . . . 
+    . 1 1 8 8 8 8 8 8 8 8 1 1 . . d 
+    . . . 8 8 8 . . 8 8 8 . . . . . 
+    . . f f f . . . . f f f . . . . 
+    `, SpriteKind.Player)
